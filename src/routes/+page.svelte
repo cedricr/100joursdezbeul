eef08d808cf29af3b508acb368ba81d6c7ed548a
Cedric Rossi <cedric@terramater.net>
2023-04-22 11:56:12 +0200

    Version Sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import parkingImg from '$lib/assets/annie-spratt-Y3te8rqlzyw-unsplash-small.jpg';
+</script>
+
+<svelte:head><title>100 jours de zbeul</title></svelte:head>
+
+<h1>100 jours de zbeul</h1>
+<p class="text-center mb-6">Très bientôt…</p>
+<figure class="mx-auto w-fit">
+	<img
+		src={parkingImg}
+		alt="Batterie de vieilles casseroles et poêles en cuivre suspendues au mur."
+		width="320"
+		height="480"
+	/>
+	<figcaption>
+		Photo par <a href="https://unsplash.com/@anniespratt">Annie Spratt</a> on
+		<a href="https://unsplash.com/photos/Y3te8rqlzyw">Unsplash</a>
+	</figcaption>
+</figure>
---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			postcss: true
+		})
+	],
+
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+	})
+	}
+};
+
+export default config;
