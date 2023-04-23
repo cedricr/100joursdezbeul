a7ad146668f0b7dba5b10ceae012ebd3b137bad4
Cedric Rossi <cedric@terramater.net>
2023-04-23 12:35:31 +0200

    Mise en forme, amélioration des remerciements

--- src/lib/thanks.svelte
+++ src/lib/thanks.svelte
@@ -1,6 +1,6 @@
 <h2 class="mb-4 text-xl font-bold">Remerciements</h2>
 
-<ul class="text-center">
+<ul class="mx-auto max-w-sm text-center">
 	<li>
 		Attac pour les
 		<a
@@ -10,16 +10,19 @@
 	</li>
 	<li>
 		<a href="https://nitter.net/Teddyruptif/">@Teddyruptif</a> pour son superbe
-		<a href="https://nitter.net/Teddyruptif/status/1649460414676172803">logo</a>&nbsp;:<br /> Parodie
-		du logo des Jeux Olympiques (cinq anneaux bleu, jaune, noir, vert, et rouge entrecroisés) les anneaux
-		ont un manche qui en fait des casseroles.
+		<a href="https://nitter.net/Teddyruptif/status/1649460414676172803">logo</a>&nbsp;, une parodie
+		du logo des Jeux Olympiques (cinq anneaux bleu, jaune, noir, vert, et rouge entrecroisés) dont
+		les anneaux ont un manche qui en fait des casseroles.
 	</li>
-	<li>Toustes les camarades qui ont contribué ou encouragé ce projet&nbsp;!</li>
+	<li>
+		Toustes les camarades de Solidaires Informatique et d'ailleurs qui ont contribué ou encouragé ce
+		projet&nbsp;!
+	</li>
 	<li><a href="https://solidairesinformatique.org">Syndiquez-vous&nbsp;!</a></li>
 </ul>
 
 <style lang="postcss">
 	li {
-		@apply mb-1 text-base;
+		@apply mb-3 text-base;
 	}
 </style>
