b9c5d944791bccab7c25244ee3e2600674859476
Cedric Rossi <cedric@terramater.net>
2023-04-26 19:56:24 +0200

    Ajout du logo SI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.postcss';
 	import logoJO from '$lib/assets/logo-jo-small.jpg';
+	import logoSI from '$lib/assets/logo-si.png';
 </script>
 
 <header role="banner">
@@ -18,10 +19,17 @@
 	<p class="zbeul mb-6 text-center">Quels sont les départements qui zbeulent le plus&nbsp;?</p>
 </header>
 <slot />
-<footer class="mt-8" role="contentinfo">
-	<hr class="mb-2" />
+<footer class="my-8" role="contentinfo">
+	<hr class="mb-4" />
+
 	<p class="text-center">
 		<a href="https://github.com/cedricr/100joursdezbeul">Code source</a> ·
 		<a href="mailto:100joursdezbeul@solidairesinformatique.org">Contact</a>
 	</p>
+
+	<div class="mx-auto mt-8 w-fit">
+		<a href="https://solidairesinformatique.org"
+			><img alt="Solidaires Informatique" src={logoSI} width="200" height="73" />
+		</a>
+	</div>
 </footer>
