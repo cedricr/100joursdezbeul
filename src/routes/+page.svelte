33e255d14709d0fdd0d3393e895100d51fc67c93
Julie Moynat <6296770+juliemoynat@users.noreply.github.com>
2023-04-29 18:25:32 +0200

    Fix accessibilité liste de résultats : passage en tableau (#58)
    * résultats en tableau
    * 3 liens sous le tableau en liste

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,13 +7,14 @@
 
 	const append = (a, x) => a.concat([x]);
 
-	const resultLines = LEADERBOARD
-		.map(([code, score]) => ({code, score}))
-		.reduce((acc, x, i) => append(acc,
-		{
-			...x,
-			rank: i > 0 && x.score === acc[i - 1].score ? acc[i - 1].rank : i + 1
-		}), []);
+	const resultLines = LEADERBOARD.map(([code, score]) => ({ code, score })).reduce(
+		(acc, x, i) =>
+			append(acc, {
+				...x,
+				rank: i > 0 && x.score === acc[i - 1].score ? acc[i - 1].rank : i + 1
+			}),
+		[]
+	);
 
 	const now = new Date();
 	const formattedDate = now.toLocaleDateString('fr', { dateStyle: 'medium' });
@@ -34,39 +35,64 @@
 	<p class="mb-6 text-center italic">
 		Cliquez sur le nom du département pour avoir le détail du décompte.
 	</p>
-	<div class="mx-auto mb-6 mt-10 max-w-sm text-xl">
-		<div class="ranking grid-auto-1-auto grid gap-x-2 gap-y-3">
-			{#each resultLines as {code, score, rank}}
-				<div class="text-right">
-					{#if rank === 1}
-						<span role="img" aria-label="1">🥇</span>
-					{:else if rank === 2}
-						<span role="img" aria-label="2">🥈</span>
-					{:else if rank === 3}
-						<span role="img" aria-label="3">🥉</span>
-					{:else}
-						{rank}
-					{/if}
-				</div>
-				<a href="/departement/{code}" class="no-underline hover:underline">
-					{getDepartmentName(code)}
-				</a>
-				<div class="text-right">{score} pts</div>
-			{/each}
-		</div>
+	<div class="mx-auto mb-6 mt-10 max-w-md text-xl">
+		<table class="ranking">
+			<thead>
+				<tr>
+					<th scope="col" class="p-1 sm:p-2">Département</th>
+					<th scope="col" class="p-1 text-center sm:p-2">Rang</th>
+					<th scope="col" class="p-1 text-center sm:p-2">Points</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each resultLines as { code, score, rank }}
+					<tr class="ranking-line">
+						<th scope="row" class="p-1 sm:p-2">
+							<a href="/departement/{code}" class="ranking-link no-underline hover:underline">
+								{getDepartmentName(code)}
+							</a>
+						</th>
+						<td class="p-1 text-center sm:p-2">
+							{#if rank === 1}
+								<span role="img" aria-label="1">🥇</span>
+							{:else if rank === 2}
+								<span role="img" aria-label="2">🥈</span>
+							{:else if rank === 3}
+								<span role="img" aria-label="3">🥉</span>
+							{:else}
+								{rank}
+							{/if}
+						</td>
+						<td class="p-1 text-right sm:p-2">
+							{score} pts<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								class="icon-link"
+								aria-hidden="true"
+								><path
+									d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z"
+								/></svg
+							>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
 	</div>
 
-	<p class="mb-20 text-center text-lg">
-		<a href="/regles-du-jeu">Règles du jeu</a>
-		<br />
-		<a href="/comment-participer">Comment participer</a> <br />
-		<a href="/presse">Revue de presse</a>
-		<br />
-		<a
-			href="https://framaforms.org/100-jours-de-zbeul-proposer-un-evenement-1682372493"
-			class="font-bold">Signaler une action</a
-		>
-	</p>
+	<ul class="mb-20 text-center text-lg">
+		<li><a href="/regles-du-jeu">Règles du jeu</a></li>
+		<li><a href="/comment-participer">Comment participer</a></li>
+		<li><a href="/presse">Revue de presse</a></li>
+		<li>
+			<a
+				href="https://framaforms.org/100-jours-de-zbeul-proposer-un-evenement-1682372493"
+				class="font-bold">Signaler une action</a
+			>
+		</li>
+	</ul>
 
 	<Thanks />
 </main>
@@ -76,11 +102,53 @@
 		@apply text-[#dd0220];
 	}
 
-	.grid-auto-1-auto {
-		grid-template-columns: auto 1fr auto;
+	.ranking {
+		width: 100%;
+		text-align: left;
 	}
 
-	.ranking > :nth-child(-n + 9) {
+	.ranking thead {
+		background-color: #e5e7eb;
+	}
+
+	.ranking tr {
+		position: relative;
+	}
+
+	.ranking th,
+	.ranking td {
+		border-width: 1px 0;
+		border-style: solid;
+		border-color: #e5e7eb;
+	}
+
+	.ranking th:last-child,
+	.ranking td:last-child {
+		min-width: 7rem;
+	}
+
+	.ranking-line th {
+		@apply font-normal;
+	}
+
+	/* Le podium */
+	.ranking .ranking-line:nth-child(-n + 3),
+	.ranking .ranking-line:nth-child(-n + 3) th {
 		@apply font-bold;
+	}
+
+	/* Agrandi les médailles */
+	.ranking .ranking-line:nth-child(-n + 3) > :nth-child(2) {
+		font-size: 125%;
+	}
+
+	/* Étend la zone cliquable du lien à toute la ligne */
+	.ranking-link::before {
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		content: '';
 	}
 </style>
