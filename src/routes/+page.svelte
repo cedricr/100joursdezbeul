e5df7fe817fe748282c8ec3a69b8458a8b0f6aa7
Pierre Flaudias <pierre.flaudias@gmail.com>
2023-05-02 13:14:26 +0200

    Ajout d'un tableau et d'une page par région

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,22 @@
 
 	import Thanks from '$lib/thanks.svelte';
 
-	import { LEADERBOARD, METADATA, getDayNumber, getDepartmentName } from '$lib/utils';
+	import { LEADERBOARD, LEADERBOARD_REGION, METADATA, getDayNumber, getDepartmentName, getRegionName } from '$lib/utils';
 
 	const dayNumber = getDayNumber();
 
 	const append = (a, x) => a.concat([x]);
 
 	const resultLines = LEADERBOARD.map(([code, score]) => ({ code, score })).reduce(
+		(acc, x, i) =>
+			append(acc, {
+				...x,
+				rank: i > 0 && x.score === acc[i - 1].score ? acc[i - 1].rank : i + 1
+			}),
+		[]
+	);
+
+	const resultLinesRegion = LEADERBOARD_REGION.map(([code, score]) => ({ code, score })).reduce(
 		(acc, x, i) =>
 			append(acc, {
 				...x,
@@ -45,53 +54,101 @@
 	<p class="mb-6 text-center italic">
 		Cliquez sur le nom du département pour avoir le détail du décompte.
 	</p>
-	<div class="mx-auto mb-6 mt-10 max-w-md text-xl">
+	<div class="mx-auto mb-6 mt-10 max-w-5xl text-xl flex justify-between items-start flex-wrap">
 		<table class="ranking">
 			<thead>
-				<tr>
-					<th scope="col" class="p-1 sm:p-2">Département</th>
-					<th scope="col" class="p-1 text-center sm:p-2">Rang</th>
-					<th scope="col" class="p-1 text-center sm:p-2">Points</th>
-				</tr>
+			<tr>
+				<th scope="col" class="p-1 sm:p-2">Région</th>
+				<th scope="col" class="p-1 text-center sm:p-2">Rang</th>
+				<th scope="col" class="p-1 text-center sm:p-2">Points</th>
+			</tr>
 			</thead>
 			<tbody>
-				{#each resultLines as { code, score, rank }}
-					<tr class="ranking-line">
-						<th scope="row" class="p-1 sm:p-2">
-							<a
-								href="/departement/{code}"
-								class="ranking-link no-underline hover:underline"
-								class:font-bold={rank < 4}
-							>
-								{getDepartmentName(code)}
-							</a>
-						</th>
-						<td class="p-1 text-center sm:p-2">
-							{#if rank === 1}
-								<img role="img" src={medailleOr} alt="1 (médaille d’or)" class="m-auto" />
-							{:else if rank === 2}
-								<img role="img" src={medailleArgent} alt="2 (médaille d’argent)" class="m-auto" />
-							{:else if rank === 3}
-								<img role="img" src={medailleBronze} alt="3 (médaille de bronze)" class="m-auto" />
-							{:else}
-								{rank}
-							{/if}
-						</td>
-						<td class="p-1 text-right sm:p-2" class:font-bold={rank < 4}>
-							{score} pts<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								class="icon-link"
-								aria-hidden="true"
-								><path
-									d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z"
-								/></svg
-							>
-						</td>
-					</tr>
-				{/each}
+			{#each resultLinesRegion as { code, score, rank }}
+				<tr class="ranking-line">
+					<th scope="row" class="p-1 sm:p-2">
+						<a
+							href="/region/{code}"
+							class="ranking-link no-underline hover:underline"
+							class:font-bold={rank < 4}
+						>
+							{getRegionName(code)}
+						</a>
+					</th>
+					<td class="p-1 text-center sm:p-2">
+						{#if rank === 1}
+							<img role="img" src={medailleOr} alt="1 (médaille d’or)" class="m-auto" />
+						{:else if rank === 2}
+							<img role="img" src={medailleArgent} alt="2 (médaille d’argent)" class="m-auto" />
+						{:else if rank === 3}
+							<img role="img" src={medailleBronze} alt="3 (médaille de bronze)" class="m-auto" />
+						{:else}
+							{rank}
+						{/if}
+					</td>
+					<td class="p-1 text-right sm:p-2" class:font-bold={rank < 4}>
+						{score} pts<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						class="icon-link"
+						aria-hidden="true"
+					><path
+						d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z"
+					/></svg
+					>
+					</td>
+				</tr>
+			{/each}
+			</tbody>
+		</table>
+		<table class="ranking">
+			<thead>
+			<tr>
+				<th scope="col" class="p-1 sm:p-2">Département</th>
+				<th scope="col" class="p-1 text-center sm:p-2">Rang</th>
+				<th scope="col" class="p-1 text-center sm:p-2">Points</th>
+			</tr>
+			</thead>
+			<tbody>
+			{#each resultLines as { code, score, rank }}
+				<tr class="ranking-line">
+					<th scope="row" class="p-1 sm:p-2">
+						<a
+							href="/departement/{code}"
+							class="ranking-link no-underline hover:underline"
+							class:font-bold={rank < 4}
+						>
+							{getDepartmentName(code)}
+						</a>
+					</th>
+					<td class="p-1 text-center sm:p-2">
+						{#if rank === 1}
+							<img role="img" src={medailleOr} alt="1 (médaille d’or)" class="m-auto" />
+						{:else if rank === 2}
+							<img role="img" src={medailleArgent} alt="2 (médaille d’argent)" class="m-auto" />
+						{:else if rank === 3}
+							<img role="img" src={medailleBronze} alt="3 (médaille de bronze)" class="m-auto" />
+						{:else}
+							{rank}
+						{/if}
+					</td>
+					<td class="p-1 text-right sm:p-2" class:font-bold={rank < 4}>
+						{score} pts<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						class="icon-link"
+						aria-hidden="true"
+					><path
+						d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z"
+					/></svg
+					>
+					</td>
+				</tr>
+			{/each}
 			</tbody>
 		</table>
 	</div>
@@ -117,8 +174,10 @@
 	}
 
 	.ranking {
-		width: 100%;
+		@apply w-full;
+		@apply md:w-5/12;
 		text-align: left;
+		margin-bottom: 3rem;
 	}
 
 	.ranking thead {
