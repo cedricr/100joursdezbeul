bfd8354993b632d3cbe815fa690ae4398f539249
Cedric Rossi <cedric@terramater.net>
2023-05-07 22:37:34 +0200

    Ajout d'un message qd il n'y a pas d'evenements

--- src/routes/date/[date]/+page.svelte
+++ src/routes/date/[date]/+page.svelte
@@ -27,17 +27,21 @@
 <h2 class="zbeul mb-16 mt-16 text-4xl">Le zbeul du {formattedDate}</h2>
 
 <div class="mx-auto mb-24 mt-10 max-w-2xl text-xl">
-	<ul>
-		{#each DEPARTMENTS.map((d) => d.code) as deptCode}
-			{@const deptEvents = filterEventsForDepartment(deptCode, events)}
-			{#if deptEvents.length}
-				<h3 class="zbeul mb-6 mt-10 text-2xl">
-					{getDepartmentName(deptCode)}&nbsp;: +{@html getPointsDisplay(getScore(deptCode))}
-				</h3>
-				{#each deptEvents as event}
-					<Event {event} hideDate />
-				{/each}
-			{/if}
-		{/each}
-	</ul>
+	{#if events.length}
+		<ul>
+			{#each DEPARTMENTS.map((d) => d.code) as deptCode}
+				{@const deptEvents = filterEventsForDepartment(deptCode, events)}
+				{#if deptEvents.length}
+					<h3 class="zbeul mb-6 mt-10 text-2xl">
+						{getDepartmentName(deptCode)}&nbsp;: +{@html getPointsDisplay(getScore(deptCode))}
+					</h3>
+					{#each deptEvents as event}
+						<Event {event} hideDate />
+					{/each}
+				{/if}
+			{/each}
+		</ul>
+	{:else}
+		Aucun événement pris en compte ce jour-là.
+	{/if}
 </div>
