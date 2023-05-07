77a4714a7b89d70762a2b62f54ce4aaef6cd42f4
Julie Moynat <6296770+juliemoynat@users.noreply.github.com>
2023-05-07 19:08:17 +0200

    Calendrier statique

---
+++ src/routes/calendrier/+page.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Calendrier du zbeul national | 100 jours de zbeul</title>
+</svelte:head>
+
+<main role="main">
+	<h2 class="zbeul mb-16 mt-16 text-4xl">Calendrier du zbeul national</h2>
+
+	<div class="mx-auto mb-24 mt-10 max-w-4xl text-xl">
+		<h3 class="text-3xl font-bold">Mai</h3>
+		<div class="xs:grid-cols-1 mb-6 mt-4 grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+			<div class="bg-slate-100">
+				<h4 class="bg-[#254093] p-3 text-center font-bold text-white">
+					<a href="">Lundi 8 mai</a>
+				</h4>
+				<div class="p-4 text-base">
+					<p>En attente de comptage :</p>
+					<ul class="list-disc pl-5">
+						<li>Casserolade à Nantes</li>
+						<li>Visite de bidule ministre à Paris</li>
+					</ul>
+				</div>
+			</div>
+			<div class="bg-slate-100">
+				<h4 class="bg-[#DC0B21] p-3 text-center font-bold text-white">
+					<a href="">Dimanche 7 mai</a>
+				</h4>
+				<div class="p-4 text-base">
+					<p>En attente de comptage :</p>
+					<ul class="list-disc pl-5">
+						<li>Casserolade à Nantes</li>
+						<li>Visite de bidule ministre à Paris</li>
+					</ul>
+				</div>
+			</div>
+			<div class="bg-slate-100">
+				<h4 class="bg-slate-600 p-3 text-center font-bold text-white">
+					<a href="">Samedi 6 mai</a>
+				</h4>
+				<div class="p-4 text-base">
+					<p>33 points !</p>
+				</div>
+			</div>
+			<div class="bg-slate-100">
+				<h4 class="bg-slate-600 p-3 text-center font-bold text-white">
+					<a href="">Vendredi 5 mai</a>
+				</h4>
+				<div class="p-4 text-base">
+					<p>33 points !</p>
+				</div>
+			</div>
+			<div class="bg-slate-100">
+				<h4 class="bg-slate-600 p-3 text-center font-bold text-white">
+					<a href="">Jeudi 4 mai</a>
+				</h4>
+				<div class="p-4 text-base">
+					<p>33 points !</p>
+				</div>
+			</div>
+			<div class="bg-slate-100">
+				<h4 class="bg-slate-600 p-3 text-center font-bold text-white">
+					<a href="">Mercredi 3 mai</a>
+				</h4>
+				<div class="p-4 text-base">
+					<p>33 points !</p>
+				</div>
+			</div>
+			<div class="bg-slate-100">
+				<h4 class="bg-slate-600 p-3 text-center font-bold text-white">
+					<a href="">Mardi 2 mai</a>
+				</h4>
+				<div class="p-4 text-base">
+					<p>33 points !</p>
+				</div>
+			</div>
+			<div class="bg-slate-100">
+				<h4 class="bg-slate-600 p-3 text-center font-bold text-white">
+					<a href="">Lundi 1<sup>er</sup> mai</a>
+				</h4>
+				<div class="p-4 text-base">
+					<p class="mb-2 text-center font-bold">🎉 Top 10 !</p>
+					<p>200 points !</p>
+				</div>
+			</div>
+		</div>
+		<!-- /.grid-->
+
+		<h3 class="text-3xl font-bold">Avril</h3>
+		…
+	</div>
+</main>
