e55cb1ac38e30d9598ddd6c825a7b859f8366f00
Julie Moynat <6296770+juliemoynat@users.noreply.github.com>
2023-05-08 10:47:20 +0200

    Ajout page mentions légales

---
+++ src/routes/mentions-legales/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+</script>
+
+<svelte:head><title>Mentions légales | 100 jours de zbeul</title></svelte:head>
+
+<div class="mx-auto mt-16 max-w-2xl">
+	<h2 class="zbeul mb-16 mt-16 text-4xl">Mentions légales</h2>
+
+	<h3>Gestion du site</h3>
+	<p>
+		<a href="https://solidairesinformatique.org/">Solidaires Informatiques</a><br />
+		31 rue de la Grange aux Belles<br />
+		75010 Paris
+	</p>
+	<p><a href="mailto:100joursdezbeul@solidairesinformatique.org">Contact par mail</a></p>
+
+	<h3>Hébergement du site</h3>
+	<p>
+		Google Cloud France SARL<br />
+		8 Rue de Londres<br />
+		75009 Paris
+	</p>
+
+	<h3>Cookies</h3>
+	<p>Ce site web n’utilise pas de cookies.</p>
+</div>
+
+<style lang="postcss">
+	h2 {
+		@apply mb-6 mt-12;
+	}
+	h3 {
+		@apply mb-8 mt-12 text-2xl font-bold;
+	}
+
+	p {
+		@apply mb-3 text-lg;
+	}
+	ul {
+		@apply mx-auto mb-6 max-w-lg list-disc pl-4;
+	}
+	li {
+		@apply mb-3 text-lg;
+	}
+</style>
