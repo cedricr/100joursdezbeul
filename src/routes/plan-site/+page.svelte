2d3146bc026f7e11bcf53d5b47cc556f62d18c91
Julie Moynat <6296770+juliemoynat@users.noreply.github.com>
2023-05-08 11:24:25 +0200

    fix retours Tailwind

--- src/routes/plan-site/+page.svelte
+++ src/routes/plan-site/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="mx-auto mt-16 max-w-2xl">
 	<h2 class="zbeul mb-16 mt-16 text-4xl">Plan du site</h2>
-	<ul>
+	<ul class="mx-auto mb-6 max-w-lg list-disc pl-4">
 		<li><a href="/">Accueil</a></li>
 		<li><a href="/nouveautes">Nouveautés du jour dans le classement</a></li>
 		<li><a href="/regles-du-jeu">Règles du jeu</a></li>
@@ -15,19 +15,6 @@
 </div>
 
 <style lang="postcss">
-	h2 {
-		@apply mb-6 mt-12;
-	}
-	h3 {
-		@apply mb-8 mt-12 text-2xl;
-	}
-
-	p {
-		@apply mb-3 text-lg;
-	}
-	ul {
-		@apply mx-auto mb-6 max-w-lg list-disc pl-4;
-	}
 	li {
 		@apply mb-3 text-lg;
 	}
