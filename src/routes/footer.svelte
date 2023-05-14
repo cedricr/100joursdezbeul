11c0c2622918cba04943fd18d5dbcc944303b994
Julie Moynat <6296770+juliemoynat@users.noreply.github.com>
2023-05-14 12:21:16 +0200

    Footer : retrait espace insécable des mots + ajout effet survol réseaux sociaux au focus aussi

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -9,11 +9,11 @@
 		<p class="mb-2 font-bold">Vous pouvez nous retrouver sur :</p>
 		<ul class="flex justify-center gap-3">
 			<li>
-				<a href="https://syndicat.solidaires.org/@SolInfoNat" rel="me"
-					><svg
+				<a href="https://syndicat.solidaires.org/@SolInfoNat" rel="me" class="inline-block">
+					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 40 40"
-						class="mastodon inline-block w-8"
+						class="mastodon w-8"
 						role="img"
 						aria-labelledby="solidaires-informatique-mastodon-title"
 					>
@@ -22,15 +22,15 @@
 						><path
 							d="M39.5509 13.4038C39.5509 4.94663 33.6041 2.46931 33.6041 2.46931C30.6067 1.18679 25.4589 0.646888 20.1104 0.605957H19.9786C14.63 0.646888 9.48647 1.18679 6.48693 2.46931C6.48693 2.46931 0.54013 4.94858 0.54013 13.4038C0.54013 15.3393 0.500387 17.6549 0.565231 20.1107C0.780679 28.3808 2.1926 36.532 10.3985 38.5552C14.1824 39.4888 17.4309 39.6837 20.0455 39.5493C24.7896 39.3037 27.4524 37.9724 27.4524 37.9724L27.2955 34.7642C27.2955 34.7642 23.9048 35.7602 20.0978 35.6393C16.3243 35.5185 12.3438 35.2612 11.733 30.9439C11.6753 30.5427 11.6474 30.1383 11.6493 29.7335C11.6493 29.7335 15.3517 30.5775 20.0455 30.7782C22.9154 30.901 25.6054 30.6223 28.3393 30.3183C33.5811 29.7355 38.1453 26.726 38.7184 23.9758C39.6262 19.643 39.5509 13.4038 39.5509 13.4038ZM32.5374 24.2994H28.1845V14.3609C28.1845 12.2656 27.239 11.2033 25.346 11.2033C23.2543 11.2033 22.2063 12.4644 22.2063 14.9592V20.3992H17.8785V14.9592C17.8785 12.4644 16.8305 11.2033 14.7367 11.2033C12.8437 11.2033 11.8982 12.2675 11.8982 14.3609V24.2994H7.54534V14.0607C7.54534 11.9673 8.11638 10.3047 9.26684 9.07486C10.4508 7.84497 12.0007 7.21345 13.9272 7.21345C16.1549 7.21345 17.8429 8.01064 18.9578 9.60697L20.0414 11.3007L21.127 9.60697C22.2419 8.01064 23.9299 7.21345 26.1576 7.21345C28.082 7.21345 29.634 7.84497 30.818 9.07486C31.9663 10.3047 32.5374 11.9673 32.5374 14.0607V24.2994Z"
 						/>
-					</svg></a
-				>
+					</svg>
+				</a>
 			</li>
 			<li>
-				<a href="https://twitter.com/SolInfoNat"
-					><svg
+				<a href="https://twitter.com/SolInfoNat" class="inline-block">
+					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 40 40"
-						class="twitter inline-block w-8"
+						class="twitter w-8"
 						role="img"
 						aria-labelledby="solidaires-informatique-twitter-title"
 						><title id="solidaires-informatique-twitter-title"
@@ -38,15 +38,15 @@
 						><path
 							d="M12.5836 36.25C27.6742 36.25 35.9305 23.7445 35.9305 12.9031C35.9305 12.5515 35.9227 12.1922 35.907 11.8406C37.5131 10.6791 38.8992 9.24042 40 7.59216C38.5042 8.25767 36.9161 8.6923 35.2898 8.88123C37.0021 7.85485 38.2842 6.24245 38.8984 4.34294C37.2876 5.2976 35.526 5.97102 33.6891 6.33435C32.4514 5.01927 30.815 4.14853 29.0328 3.85675C27.2506 3.56498 25.422 3.86842 23.8296 4.72016C22.2372 5.5719 20.9697 6.92451 20.2231 8.56885C19.4765 10.2132 19.2924 12.0577 19.6992 13.8172C16.4375 13.6535 13.2466 12.8062 10.3333 11.3302C7.42004 9.85415 4.84949 7.78239 2.78828 5.24919C1.74067 7.05541 1.42009 9.19274 1.89172 11.2268C2.36334 13.2609 3.59177 15.0391 5.32734 16.2C4.02438 16.1586 2.74996 15.8078 1.60938 15.1765V15.2781C1.60821 17.1736 2.2635 19.011 3.46385 20.4779C4.6642 21.9449 6.33554 22.9509 8.19375 23.325C6.98676 23.6552 5.71997 23.7033 4.49141 23.4656C5.01576 25.0957 6.03596 26.5215 7.40962 27.5439C8.78328 28.5663 10.4419 29.1343 12.1539 29.1687C9.24737 31.4518 5.65696 32.6902 1.96094 32.6844C1.30548 32.6833 0.650665 32.6432 0 32.564C3.75476 34.9729 8.12255 36.2523 12.5836 36.25Z"
 						/>
-					</svg></a
-				>
+					</svg>
+				</a>
 			</li>
 			<li>
-				<a href="https://www.youtube.com/channel/UCge6n8ryVZR076ZEyOJP00Q"
-					><svg
+				<a href="https://www.youtube.com/channel/UCge6n8ryVZR076ZEyOJP00Q" class="inline-block">
+					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 40 40"
-						class="youtube inline-block w-8"
+						class="youtube w-8"
 						role="img"
 						aria-labelledby="solidaires-informatique-youtube-title"
 						><title id="solidaires-informatique-youtube-title"
@@ -54,15 +54,15 @@
 						><path
 							d="M39.6016 12C39.6016 12 39.2109 9.24219 38.0078 8.03125C36.4844 6.4375 34.7813 6.42969 34 6.33594C28.4063 5.92969 20.0078 5.92969 20.0078 5.92969H19.9922C19.9922 5.92969 11.5937 5.92969 6 6.33594C5.21875 6.42969 3.51562 6.4375 1.99219 8.03125C0.789063 9.24219 0.40625 12 0.40625 12C0.40625 12 0 15.2422 0 18.4766V21.5078C0 24.7422 0.398437 27.9844 0.398437 27.9844C0.398437 27.9844 0.789062 30.7422 1.98437 31.9531C3.50781 33.5469 5.50781 33.4922 6.39844 33.6641C9.60156 33.9688 20 34.0625 20 34.0625C20 34.0625 28.4063 34.0469 34 33.6484C34.7813 33.5547 36.4844 33.5469 38.0078 31.9531C39.2109 30.7422 39.6016 27.9844 39.6016 27.9844C39.6016 27.9844 40 24.75 40 21.5078V18.4766C40 15.2422 39.6016 12 39.6016 12ZM15.8672 25.1875V13.9453L26.6719 19.5859L15.8672 25.1875Z"
 						/>
-					</svg></a
-				>
+					</svg>
+				</a>
 			</li>
 			<li>
-				<a href="https://www.twitch.tv/solidaires_info"
-					><svg
+				<a href="https://www.twitch.tv/solidaires_info" class="inline-block">
+					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 40 40"
-						class="twitch inline-block w-8"
+						class="twitch w-8"
 						role="img"
 						aria-labelledby="solidaires-informatique-twitch-title"
 						><title id="solidaires-informatique-twitch-title"
@@ -73,15 +73,15 @@
 						/>
 						<path d="M30.0012 7.85718H27.144V16.4286H30.0012V7.85718Z" />
 						<path d="M22.144 7.85718H19.2869V16.4286H22.144V7.85718Z" />
-					</svg></a
-				>
+					</svg>
+				</a>
 			</li>
 			<li>
-				<a href="https://www.facebook.com/solidairesinformatique/"
-					><svg
+				<a href="https://www.facebook.com/solidairesinformatique/" class="inline-block">
+					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 40 40"
-						class="facebook inline-block w-8"
+						class="facebook w-8"
 						role="img"
 						aria-labelledby="solidaires-informatique-facebook-title"
 						><title id="solidaires-informatique-facebook-title"
@@ -90,20 +90,18 @@
 						<path
 							d="M40 20C40 8.9543 31.0457 0 20 0C8.9543 0 0 8.9543 0 20C0 29.9824 7.31367 38.2566 16.875 39.757V25.7813H11.7969V20H16.875V15.5938C16.875 10.5813 19.8609 7.8125 24.4293 7.8125C26.6168 7.8125 28.9062 8.20313 28.9062 8.20313V13.125H26.3844C23.9 13.125 23.125 14.6668 23.125 16.25V20H28.6719L27.7852 25.7813H23.125V39.757C32.6863 38.2566 40 29.9824 40 20Z"
 						/>
-					</svg></a
-				>
+					</svg>
+				</a>
 			</li>
 		</ul>
 	</div>
 
 	<ul class="text-center">
 		<li class="inline">
-			<a href="/plan-site">Plan&nbsp;du&nbsp;site</a><span aria-hidden="true">&nbsp;·</span>
+			<a href="/plan-site">Plan du site</a><span aria-hidden="true">&nbsp;·</span>
 		</li>
 		<li class="inline">
-			<a href="https://github.com/cedricr/100joursdezbeul">Code&nbsp;source</a><span
-				aria-hidden="true"
-			>
+			<a href="https://github.com/cedricr/100joursdezbeul">Code source</a><span aria-hidden="true">
 				&nbsp;·</span
 			>
 		</li>
@@ -114,7 +112,7 @@
 			</span>
 		</li>
 		<li class="inline">
-			<a href="/mentions-legales">Mentions&nbsp;légales</a>
+			<a href="/mentions-legales">Mentions légales</a>
 		</li>
 	</ul>
 
@@ -126,23 +124,28 @@
 </footer>
 
 <style lang="postcss">
-	li:hover .mastodon {
+	a:hover .mastodon,
+	a:focus .mastodon {
 		fill: #6364ff;
 	}
 
-	li:hover .twitter {
+	a:hover .twitter,
+	a:focus .twitter {
 		fill: #1da1f2;
 	}
 
-	li:hover .youtube {
+	a:hover .youtube,
+	a:focus .youtube {
 		fill: #ff0000;
 	}
 
-	li:hover .twitch {
+	a:hover .twitch,
+	a:focus .twitch {
 		fill: #9146ff;
 	}
 
-	li:hover .facebook {
+	a:hover .facebook,
+	a:focus .facebook {
 		fill: #1877f2;
 	}
 </style>
