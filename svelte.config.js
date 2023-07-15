85222f525a4378262176bede42ee5b55163bc361
Cedric Rossi <cedric@terramater.net>
2023-07-15 16:10:20 +0200

    Migration vers Github Pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
@@ -17,7 +19,10 @@ const config = {
 			assets: 'build',
 			fallback: null,
 			precompress: false,
-			strict: true
+			strict: true,
+			paths: {
+				base: dev ? '' : process.env.BASE_PATH
+			}
 		})
 	},
 	onwarn(warning, defaultHandler) {
