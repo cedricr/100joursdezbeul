32173bb369c936eeb1d59b236c5b54248b945168
Cedric Rossi <cedric@terramater.net>
2023-07-15 17:46:32 +0200

    Nettoyage

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,6 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
@@ -19,10 +17,7 @@ const config = {
 			assets: 'build',
 			fallback: null,
 			precompress: false,
-			strict: true,
-			paths: {
-				base: dev ? '' : process.env.BASE_PATH
-			}
+			strict: true
 		})
 	},
 	onwarn(warning, defaultHandler) {
